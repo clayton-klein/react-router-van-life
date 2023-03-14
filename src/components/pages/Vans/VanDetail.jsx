ea2a4e067573c751404d15fa511ad07a2d0ed480
clayton-klein <csklein87@hotmail.com>
2023-03-14 19:31:09 -0300

    chore: create HostLayout.css

--- src/components/pages/Vans/VanDetail.jsx
+++ src/components/pages/Vans/VanDetail.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import "../styles/VanDetail.css";
+import "../../../styles/VanDetail.css";
 
 export default function VanDetail() {
   const [van, setVan] = useState(null);
