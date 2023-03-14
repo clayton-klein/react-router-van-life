97f48aa7ede4d991a13488fc89718c7d682747b1
clayton-klein <csklein87@hotmail.com>
2023-03-14 19:37:33 -0300

    Revert "chore: create HostLayout.css"
    This reverts commit ea2a4e067573c751404d15fa511ad07a2d0ed480.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Vans from "./components/Vans";
+import VanDetail from "./components/VanDetail";
 import "./server";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/pages/About";
-import Dashboard from "./components/pages/Host/Dashboard";
-import Home from "./components/pages/Home";
-import HostLayout from "./components/pages/Host/HostLayout";
-import Income from "./components/pages/Host/Income";
-import Layout from "./components/Layout";
-import Reviews from "./components/pages/Host/Reviews";
-import VanDetail from "./components/pages/Vans/VanDetail";
-import Vans from "./components/pages/Vans/Vans";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
       <Routes>
-        {/* Layout doesn't need a path */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
-          </Route>
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../../../styles/Vans.css";
+import "../styles/Vans.css";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
