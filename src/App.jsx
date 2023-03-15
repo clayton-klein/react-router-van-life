e307db8dbdfb90a5867b6f4d93175aef284cc24a
clayton-klein <csklein87@hotmail.com>
2023-03-14 21:03:00 -0300

    chore: add comment about react-router index keyword

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,20 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Layout doesn't need a path */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          {/* add index keyword to Home, because it has 
+          the same path as Layout, so itś necessary to do it
+          to render the component instead of using path="/" */}
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
 
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />}>
+            {/* same thing as above here in Dashboard */}
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
