1028102e0a5109234495d6ad1405b63f75332cf7
clayton-klein <csklein87@hotmail.com>
2023-03-22 14:30:28 -0300

    feat: add dynamic class to buttons when selected

--- src/components/pages/Vans/Vans.jsx
+++ src/components/pages/Vans/Vans.jsx
@@ -19,7 +19,8 @@ export default function Vans() {
     .filter((van) => (typeFilter ? van.type.toLowerCase() === typeFilter : van))
     .map((van) => (
       <div className="van-tile" key={van.id}>
-        <Link to={`/vans/${van.id}`}>
+        {/* the path (to) of the Link bellow is relative and the server already returns a string and state is passed so it can be used in the VanDetail page's Link */}
+        <Link to={van.id} state={{ search: `?${searchParams.toString()}` }}>
           <img src={van.imageUrl} />
           <div className="van-info">
             <h3>{van.name}</h3>
@@ -33,10 +34,20 @@ export default function Vans() {
       </div>
     ));
 
+  function addSelectedClassToBtn(btnType) {
+    const btns = document.querySelectorAll("button");
+    btns.forEach((btn) =>
+      btn.classList.contains(btnType)
+        ? btn.classList.add("selected")
+        : btn.classList.remove("selected")
+    );
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <nav className="van-list-filter-buttons">
+        {addSelectedClassToBtn(typeFilter)}
         <button
           // there are different ways of passing values into the setSearchParams, see more on https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/URLSearchParams (useSearchParams use the URL API of the browser behind under the hood).
           onClick={() => setSearchParams({ type: "simple" })}
@@ -44,24 +55,29 @@ export default function Vans() {
         >
           Simple
         </button>
+
         <button
           onClick={() => setSearchParams({ type: "rugged" })}
           className="van-type rugged"
         >
           Rugged
         </button>
+
         <button
           onClick={() => setSearchParams({ type: "luxury" })}
           className="van-type luxury"
         >
           Luxury
         </button>
-        <button
-          onClick={() => setSearchParams({})}
-          className="van-type clear-filters"
-        >
-          Clear
-        </button>
+
+        {typeFilter && (
+          <button
+            onClick={() => setSearchParams({})}
+            className="van-type clear-filters"
+          >
+            Clear Filter
+          </button>
+        )}
       </nav>
       <div className="van-list">{vansElements}</div>
     </div>
