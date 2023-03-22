9ef3d19a485632a8046be4689b132f91bab4748e
clayton-klein <csklein87@hotmail.com>
2023-03-22 14:40:19 -0300

    feat: add dynamic back button's text

--- src/components/pages/Vans/VanDetail.jsx
+++ src/components/pages/Vans/VanDetail.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 import "../../../styles/VanDetail.css";
 
 export default function VanDetail() {
   const [van, setVan] = useState(null);
   const params = useParams();
-
+  const location = useLocation();
+  console.log(location);
   useEffect(() => {
     fetch(`/api/vans/${params.id}`) // get data from mirage.js server
       .then((resp) => resp.json())
@@ -15,6 +16,18 @@ export default function VanDetail() {
 
   return (
     <div className="van-detail-container">
+      <Link
+        to={location.state ? `..${location.state.search}` : ".."} // to use this '..' (go back one level)...
+        relative="path" // we have to specify prop, otherwise it will go one level on the layout hierarchy ('/').
+        className="back-button"
+      >
+        &larr;{" "}
+        <span>
+          {location.state.search !== "?"
+            ? `Back to ${location.state.search.slice(6)}`
+            : "Back"}
+        </span>
+      </Link>
       {/* van starts as null   */}
       {van ? (
         <div className="van-detail">
