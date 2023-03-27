08948879d57978c036182ece3b3d8a1bd9134463
clayton-klein <csklein87@hotmail.com>
2023-03-27 11:52:42 -0300

    refactor: create and find user functionalities

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Model, Response } from "miragejs";
 
 createServer({
   models: {
     vans: Model,
+    users: Model,
   },
 
   seeds(server) {
@@ -37,7 +38,7 @@ createServer({
       imageUrl:
         "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png",
       type: "luxury",
-      hostId: "123",
+      hostId: "456",
     });
     server.create("van", {
       id: "4",
@@ -70,39 +71,62 @@ createServer({
       imageUrl:
         "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png",
       type: "rugged",
-      hostId: "789",
+      hostId: "123",
+    });
+    server.create("user", {
+      id: "123",
+      email: "b@b.com",
+      password: "p123",
+      name: "Bob",
     });
   },
 
   routes() {
     this.namespace = "api";
     this.logging = false;
+    // this.timing = 2000
 
     this.get("/vans", (schema, request) => {
-      // return new Response(400, {}, {error: "Error fetching data"});
+      // return new Response(400, {}, {error: "Error fetching data"})
       return schema.vans.all();
     });
 
     this.get("/vans/:id", (schema, request) => {
-      // return new Response(400, {}, {error: "Error fetching data"});
-
       const id = request.params.id;
       return schema.vans.find(id);
     });
 
     this.get("/host/vans", (schema, request) => {
-      // return new Response(400, {}, {error: "Error fetching data"});
-
       // Hard-code the hostId for now
       return schema.vans.where({ hostId: "123" });
     });
 
     this.get("/host/vans/:id", (schema, request) => {
-      // return new Response(400, {}, {error: "Error fetching data"});
-
       // Hard-code the hostId for now
       const id = request.params.id;
       return schema.vans.findBy({ id, hostId: "123" });
+    });
+
+    this.post("/login", (schema, request) => {
+      const { email, password } = JSON.parse(request.requestBody);
+      // This is an extremely naive version of authentication. Please don't
+      // do this in the real world, and never save raw text passwords
+      // in your database 😇
+      const foundUser = schema.users.findBy({ email, password });
+      if (!foundUser) {
+        return new Response(
+          401,
+          {},
+          { message: "No user with those credentials found!" }
+        );
+      }
+
+      // At the very least, don't send the password back to the client 😅
+      foundUser.password = undefined;
+      return {
+        user: foundUser,
+        token: "Enjoy your pizza, here's your tokens.",
+      };
     });
   },
 });
