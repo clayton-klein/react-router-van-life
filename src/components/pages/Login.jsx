51bbaffae17019c95dc8c12c5b1ed53f69d03fb5
clayton-klein <csklein87@hotmail.com>
2023-03-27 11:57:00 -0300

    feat: implement login functionalities

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import '../../styles/Login.css'
+import { useLoaderData } from "react-router-dom";
+import "../../styles/Login.css";
+import { loginUser } from "../../api";
+
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
 
 export default function Login() {
   const [loginFormData, setLoginFormData] = React.useState({
     email: "",
     password: "",
   });
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
+
+  const message = useLoaderData();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginFormData);
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((res) => console.log(res))
+      .catch(err => setError(err))
+      .finally(() => setStatus("idle"))
+    setError(null);
   }
 
   function handleChange(e) {
@@ -23,6 +37,8 @@ export default function Login() {
 
   return (
     <div className="login-container">
+      {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
       <h1>Sign in to your account</h1>
       <form onSubmit={handleSubmit} className="login-form">
         <input
@@ -39,7 +55,9 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
