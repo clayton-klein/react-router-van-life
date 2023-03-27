11d3aaf47a1f8f7a74a87c29d0549f39d7870af5
clayton-klein <csklein87@hotmail.com>
2023-03-27 20:11:58 -0300

    refactor: check if user is logged in and redirect

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,15 @@ import { redirect } from "react-router-dom";
 // the idea with this function was to create a protected route, but for some reason it's not working :/
 
 // using async just to pretend that it's a asynchronous function
-export async function requireAuth() {
-  // force user to be redirected to the login page
-  const isLoggedIn = false;
+export async function requireAuth(request) {
+  // console.log(request);
+  
+  const isLoggedIn = localStorage.getItem("loggedin");
+  const pathName = new URL(request.url).pathname;
 
   if (!isLoggedIn) {
-    throw redirect("/login?message=You must log in first");
+    // it should be throw instead of return, but if I use throw the app breaks at Host route (that is supposed to be protected) and idk why :/
+    return redirect(`/login?message=You must log in first.&redirectTo=${pathName}`);
   }
 
   return null;
