1cb5483290fffe131ad2bf9bd5f9780136fe896f
clayton-klein <csklein87@hotmail.com>
2023-03-28 19:03:19 -0300

    docs: tweak a comment

--- src/utils.js
+++ src/utils.js
@@ -5,12 +5,11 @@ import { redirect } from "react-router-dom";
 // using async just to pretend that it's a asynchronous function
 export async function requireAuth(request) {
   // console.log(request);
-  
-  const isLoggedIn = localStorage.getItem("loggedin");
   const pathName = new URL(request.url).pathname;
+  const isLoggedIn = localStorage.getItem("loggedin");
 
   if (!isLoggedIn) {
-    // it should be throw instead of return, but if I use throw the app breaks at Host route (that is supposed to be protected) and idk why :/
+    // it should be throw instead of return, but if I use throw the app breaks at the Host route (that was supposed to be protected) and idk why :/ itś like the redirect function just don't work!
     return redirect(`/login?message=You must log in first.&redirectTo=${pathName}`);
   }
 
